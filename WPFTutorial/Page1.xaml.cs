eaa7afaac47ebcd5289ca93feb92364f8880d4e4
LoadWithWater <60064819+LoadWithWater@users.noreply.github.com>
2024-06-17 11:37:01 +0900

    Add files via upload

---
+++ WPFTutorial/Page1.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Controls;
+
+namespace WPFTutorial
+{
+    /// <summary>
+    /// Page1.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        bool running = false;
+
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToggleRun_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (running) {
+                // stop
+                tbStatus.Text = "Stopped";
+                btnToggleRun.Content = "Run";
+            } else {
+                // run
+                tbStatus.Text = "Running";
+                btnToggleRun.Content = "Stop";
+            }
+            
+            running = !running;
+        }
+    }
+}
